326d5b20163e32d968d017fc346f083abc08e34e
Andre Fortin <andre.v.fortin@gmail.com>
2022-01-17 12:31:04 -0500

    update flutter_svg import statement on splash_screen

--- lib/screens/welcome/splash_screen.dart
+++ lib/screens/welcome/splash_screen.dart
@@ -1,6 +1,6 @@
 import 'package:chat/screens/welcome/welcome_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
